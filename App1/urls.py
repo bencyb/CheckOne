f8563e9717eb7024961b11cc5236fca8ca1db6f8
Bency Baby <bency@gmail.com>
2023-08-08 07:39:54 +0530

    first commit

--- App1/urls.py
+++ App1/urls.py
@@ -1,4 +1,15 @@
+# from django.urls import path
+# from .views import home,callback_view,callback_listener_view
+# # webhook_callback
+# from . import views
+# urlpatterns = [
+#     path('home/', home, name="home"),
+#     path('callback/', callback_view, name='callback_view'),
+#     path('callback_listener/', views.callback_listener_view, name='callback_listener'),
+#     # path('webhook/', webhook_callback, name='webhook-callback'),
 
+
+# ]
 
 
 from django.urls import path
--- App1/views.py
+++ App1/views.py
@@ -1,78 +1,3 @@
-# from rest_framework.decorators import api_view, authentication_classes, permission_classes
-# from rest_framework.response import Response
-# from rest_framework.authentication import TokenAuthentication
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework import status
-# from django.shortcuts import render
-# from .serializers import DataSerializer
-# from django.urls import reverse
-# from urllib.parse import urljoin
-# from .models import DataModel
-
-
-
-
-# def home(request):
-#     if request.method == 'POST':
-#         data = request.POST.get('data')
-#         DataModel.objects.create(data=data)
-#     return render(request, 'callback_data.html')
-
-
-
-# @api_view(['POST'])
-# def callback_view(request):
-#     serializer = DataSerializer(data=request.data)
-#     if serializer.is_valid():
-#         data = serializer.validated_data['data']
-#         callback_url_relative = reverse('callback_listener')
-#         base_url = request.build_absolute_uri('/')   
-#         callback_url_absolute = urljoin(base_url, callback_url_relative)
-
-#         response_data = {
-#             'message': "Data Triggered successfully",
-#             'callback_url': callback_url_absolute
-#         }
-
-#         return Response(response_data)
-#     else:
-#         return Response(serializer.errors, status=400)
-
-
-
-# @api_view(['GET'])
-# def callback_listener_view(request):
-#     data_from_callback = DataModel.objects.all()
-#     serializer = DataSerializer(data_from_callback, many=True)
-
-#     return Response({'message': 'Data received and saved successfully', 'data_from_callback': serializer.data})
-
-    
-
-
-
-
-
-
-# @api_view(['POST'])
-# @authentication_classes([TokenAuthentication]) 
-# @permission_classes([IsAuthenticated])  
-# def webhook_callback(request):
-#     submission_data = {
-#         "submission_id": 123,
-#         "test_result": "passed",
-#         "timestamp": "2023-08-04T12:34:56"
-#     }
-#     response_data = {
-#         "message": "Webhook received successfully.",
-#         "submission_data": submission_data
-#     }
-#     return Response(response_data, status=status.HTTP_200_OK)
-
-
-
-
-
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import JsonResponse
@@ -102,10 +27,7 @@ def callback_view(request):
         callback_url_relative = reverse('callback_listener')
         base_url = request.build_absolute_uri('/')
         callback_url_absolute = urljoin(base_url, callback_url_relative)
-
-        # You can save the data to the database if needed before triggering the callback
         DataModel.objects.create(data=data)
-
         response_data = {
             'message': "Data Triggered successfully",
             'callback_url': callback_url_absolute
@@ -114,8 +36,35 @@ def callback_view(request):
     else:
         return Response(serializer.errors, status=400)
 
-@api_view(['GET'])
+
 def callback_listener_view(request):
     data_from_callback = DataModel.objects.all()
     serializer = DataSerializer(data_from_callback, many=True)
-    return Response({'message': 'Data received and saved successfully', 'data_from_callback': serializer.data})
+
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        return render(request, 'receive_data.html', {'data_from_callback': serializer.data})
+
+
+# @api_view(['POST'])
+# @authentication_classes([TokenAuthentication]) 
+# @permission_classes([IsAuthenticated])  
+# def webhook_callback(request):
+#     submission_data = {
+#         "submission_id": 123,
+#         "test_result": "passed",
+#         "timestamp": "2023-08-04T12:34:56"
+#     }
+#     response_data = {
+#         "message": "Webhook received successfully.",
+#         "submission_data": submission_data
+#     }
+#     return Response(response_data, status=status.HTTP_200_OK)
+
+# @api_view(['GET'])
+# def callback_listener_view(request):
+#     data_from_callback = DataModel.objects.all()
+#     serializer = DataSerializer(data_from_callback, many=True)
+#     return render(request,'receive_data.html', 'data_from_callback': serializer.data)
+#     # return Response({'message': 'Data received and saved successfully', 'data_from_callback': serializer.data})
